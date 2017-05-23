0f0f47214f032d626dcbb5a228d0423fb46328ec
Paweł Pawlik <Pawel.Pawlik@euvic.pl>
2017-05-23 17:27:59 +0200

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requestet for: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit!");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	void Awake () {
+		if (!instance) {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		} else{
+			Destroy (gameObject);
+			print ("Destroyed duplicate music player");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
